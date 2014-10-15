0fa4e8ec279fbed100811aeb6a3fb55fb5ccddd1
Bermudez, Jaime <jaime_bermudez@harvard.edu>
2014-10-15 14:45:16 -0400

    Add CanvasSchoolTemplate model that stores relationship between a canvas "template" course id and a school id.  Create tests and add an api method at the data access layer to retrieve a template id for a given school.

--- canvas_course_site_wizard/models_api.py
+++ canvas_course_site_wizard/models_api.py
@@ -1,4 +1,4 @@
-from .models import SISCourseData
+from .models import SISCourseData, CanvasSchoolTemplate
 
 
 def get_course_data(course_sis_id):
@@ -9,3 +9,13 @@ def get_course_data(course_sis_id):
     exception if multiple instances match the input id.
     """
     return SISCourseData.objects.select_related('course').get(pk=course_sis_id)
+
+
+def get_template_for_school(school_code):
+    """
+    Retrieve a single course template id for the given school code.  An
+    ObjectDoesNotExist exception will be raised if the school does not have a template.
+    If there are multiple templates for the school, a MultipleObjectsReturned exception
+    will be thrown.
+    """
+    return CanvasSchoolTemplate.objects.get(school_id=school_code).template_id
