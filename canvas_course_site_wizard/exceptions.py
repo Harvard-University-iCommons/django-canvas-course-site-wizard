87e7def7c578571461cac58c286bea76e1c951f7
Yates, Elliott <elliott_yates@harvard.edu>
2014-10-23 06:04:27 -0400

    Updates enroll instructor feature with refactoring proposed by Jaime, including switching list users call to a get user profile call, creating a utility method for building a course URL, and cleaning up unit tests.

--- canvas_course_site_wizard/exceptions.py
+++ canvas_course_site_wizard/exceptions.py
@@ -7,19 +7,8 @@ class NoTemplateExistsForSchool(Exception):
 
 
 class NoCanvasUserToEnroll(Exception):
-    def __init__(self, sis_user_id, canvas_account_id):
+    def __init__(self, sis_user_id):
         self.sis_user_id = sis_user_id
-        self.canvas_account_id = canvas_account_id
 
     def __unicode__(self):
-        return u'User with sis_user_id=%s does not exist in canvas_account_id=%s' % (self.sis_user_id,
-                                                                                     self.canvas_account_id)
-
-class TooManyMatchingUsersToEnroll(Exception):
-    def __init__(self, sis_user_id, canvas_account_id):
-        self.sis_user_id = sis_user_id
-        self.canvas_account_id = canvas_account_id
-
-    def __unicode__(self):
-        return u'More than one user matching sis_user_id=%s exist in canvas_account_id=%s' % (self.sis_user_id,
-                                                                                              self.canvas_account_id)
+        return u'No Canvas user with sis_user_id=%s.' % self.sis_user_id
--- canvas_course_site_wizard/tests/test_controller_enroll_creator_in_new_course.py
+++ canvas_course_site_wizard/tests/test_controller_enroll_creator_in_new_course.py
@@ -1,24 +1,24 @@
 from unittest import TestCase
-from mock import patch
+from mock import patch, DEFAULT, ANY
 from canvas_course_site_wizard.controller import enroll_creator_in_new_course
-from canvas_course_site_wizard.exceptions import NoCanvasUserToEnroll, TooManyMatchingUsersToEnroll
+from canvas_course_site_wizard.exceptions import NoCanvasUserToEnroll
 
 
+@patch.multiple('canvas_course_site_wizard.controller', get_user_profile=DEFAULT, enroll_user_sections=DEFAULT)
 class EnrollCreateInNewCourseTest(TestCase):
 
     def setUp(self):
         self.sis_course_id = '123456'
         self.account_id = 123
         self.sis_user_id = 'sis-test-user'
+        self.canvas_user_id = '5555'
         self.course = {
             'id': 1234,
             'account_id': self.account_id,
             'sis_course_id': self.sis_course_id
-            }
+        }
 
-    @patch('canvas_course_site_wizard.controller.enroll_user_sections')
-    @patch('canvas_course_site_wizard.controller.list_users_in_account')
-    def test_user_successful_enrollment(self, list_users_mock, enroll_user_mock):
+    def test_user_successful_enrollment(self, get_user_profile, enroll_user_sections):
         """
         Test that SDK calls to user and enrollments receive expected values and that
         enroll_creator_in_new_course() returns results of enrollment call
@@ -29,34 +29,22 @@ class EnrollCreateInNewCourseTest(TestCase):
             'sis_course_id': self.sis_course_id,
             'sis_section_id': self.sis_course_id
         }
-        list_users_mock.return_value.json.return_value = [{'sis_user_id': self.sis_user_id}]
-        enroll_user_mock.return_value.json.return_value = expected_return_values
+        get_user_profile.return_value.status_code = 200
+        get_user_profile.return_value.json.return_value = {'id': self.canvas_user_id}
+        enroll_user_sections.return_value.json.return_value = expected_return_values
         result = enroll_creator_in_new_course(self.course, self.sis_user_id)
-        self.assertTrue(list_users_mock.called)
-        self.assertTrue(enroll_user_mock.called)
+        get_user_profile.assert_called_with(request_ctx=ANY, user_id='sis_user_id:%s' % self.sis_user_id)
+        enroll_user_sections.assert_called_with(request_ctx=ANY, section_id='sis_section_id:%s' % self.sis_course_id,
+                                                enrollment_user_id=self.canvas_user_id,
+                                                enrollment_type='TeacherEnrollment',
+                                                enrollment_enrollment_state='active')
         self.assertDictContainsSubset(expected_return_values, result, "result contains: %s" % result)
 
-    @patch('canvas_course_site_wizard.controller.enroll_user_sections')
-    @patch('canvas_course_site_wizard.controller.list_users_in_account')
-    def test_no_matching_users(self, list_users_mock, enroll_user_mock):
+    def test_no_user_to_enroll(self, get_user_profile, enroll_user_sections):
         """
-        Test that enrollment is not attempted if user does not exist in Canvas and that error is passed back to caller
+        Test that error is passed back to caller if user does not exist in Canvas
         """
-        list_users_mock.return_value.json.return_value = []
+        get_user_profile.return_value.status_code.return_value = 404
         with self.assertRaises(NoCanvasUserToEnroll):
             result = enroll_creator_in_new_course(self.course, self.sis_user_id)
-        self.assertTrue(list_users_mock.called)
-        self.assertFalse(enroll_user_mock.called)
-
-    @patch('canvas_course_site_wizard.controller.enroll_user_sections')
-    @patch('canvas_course_site_wizard.controller.list_users_in_account')
-    def test_multiple_matching_users(self, list_users_mock, enroll_user_mock):
-        """
-        Test that enrollment is not attempted if there is more than one user in Canvas matching
-        the SIS user ID provided, and that error is passed back to caller
-        """
-        list_users_mock.return_value.json.return_value = [{'sis_user_id': self.sis_user_id}, {'sis_user_id': 'match_2'}]
-        with self.assertRaises(TooManyMatchingUsersToEnroll):
-            result = enroll_creator_in_new_course(self.course, self.sis_user_id)
-        self.assertTrue(list_users_mock.called)
-        self.assertFalse(enroll_user_mock.called)
+        self.assertTrue(get_user_profile.called)
