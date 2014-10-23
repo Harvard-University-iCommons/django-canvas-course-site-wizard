e7934fe8f9176f35e63ad8857c3c16d5a02e73fa
Bermudez, Jaime <jaime_bermudez@harvard.edu>
2014-10-22 23:21:19 -0400

    Split permissions into two mixins: 1) One that implements the permission check methods (relying on the course data object), and 2) the other implementing the dispatch method that uses the permission checks and will raise a PermissionDenied if a combination of the checks fails.  Include mixin tests.  Add more information to NoTemplateForSchool exception.  Add newline at end of file for views.py.

--- canvas_course_site_wizard/exceptions.py
+++ canvas_course_site_wizard/exceptions.py
@@ -3,7 +3,7 @@ class NoTemplateExistsForSchool(Exception):
         self.school_id = school_id
 
     def __unicode__(self):
-        return repr(self.school_id)
+        return u'No template exists for school_id=%s' % self.school_id
 
 
 class NoCanvasUserToEnroll(Exception):
@@ -14,6 +14,7 @@ class NoCanvasUserToEnroll(Exception):
     def __unicode__(self):
         return u'User with sis_user_id=%s does not exist in canvas_account_id=%s' % (self.sis_user_id,
                                                                                      self.canvas_account_id)
+
 
 class TooManyMatchingUsersToEnroll(Exception):
     def __init__(self, sis_user_id, canvas_account_id):
