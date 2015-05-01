2bca3d8911e5aba2819f38587afd7c3bab151090
Parker, Eric P <eric_parker@harvard.edu>
2015-05-01 14:37:15 -0400

    changed url pattern to match the course create

--- canvas_course_site_wizard/urls.py
+++ canvas_course_site_wizard/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns(
     '',
     url(r'^courses/(?P<pk>\d+)/create$', CanvasCourseSiteCreateView.as_view(), name='ccsw-create'),
     url(r'^status/(?P<pk>\d+)$', CanvasCourseSiteStatusView.as_view(), name='ccsw-status'),
-    url(r'^bulk_create/(?P<pk>\d+)$', CanvasBulkCreateStatusView.as_view(), name='ccsw-bulk-create')
+    url(r'^term/(?P<pk>\d+)/bulk_create$', CanvasBulkCreateStatusView.as_view(), name='ccsw-bulk-create')
 )
