eb97f0f2f21bc4e8c81aeb51866e63fb2d85fbd6
Sapna Mysore <sapna_mysore@harvard.edu>
2019-09-12 12:50:06 -0400

    Removed dependency on shopping fields

--- canvas_course_site_wizard/models.py
+++ canvas_course_site_wizard/models.py
@@ -86,14 +86,14 @@ class SISCourseDataMixin(object):
         """
         return self.course.school_id
 
-    @property
-    def shopping_active(self):
-        """
-        Shopping status of the course; ie the course is shoppable (returns True) if it is in a term for which shopping
-         is turned on and the course is not explicitly excluded from shopping.
-        :returns: boolean
-        """
-        return self.term.shopping_active and not self.exclude_from_shopping
+    # @property
+    # def shopping_active(self):
+    #     """
+    #     Shopping status of the course; ie the course is shoppable (returns True) if it is in a term for which shopping
+    #      is turned on and the course is not explicitly excluded from shopping.
+    #     :returns: boolean
+    #     """
+    #     return self.term.shopping_active and not self.exclude_from_shopping
 
     def get_official_course_site_url(self):
         """
